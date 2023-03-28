fb93bdedb36efbf8732b9e9ab90c9d1bcd73e8df
Samuel HAdden <hadden@tpb12.sunnyvale>
2023-03-28 12:21:22 -0400

    first megno grid run

--- code/DiffusionExperiment.py
+++ code/DiffusionExperiment.py
@@ -1,14 +1,22 @@
 from celmech.maps import CometMap
 from DiffusionExperimentUtils import *
 import numpy as np
+import sys
+TEST = False
+I = int(sys.argv[1])
 
-Ntraj = 500
-Tfin = 1e9
-Npts = 5_000
+if TEST:
+    Ntraj = 10
+    Tfin  = 1e6
+    Npts  = 10
+else:
+    Ntraj = 500
+    Tfin  = 1e9
+    Npts  = 10_000
 
 
 muN = 5.15e-5
-aN = 30. 
+aN = 30.
 q = 35.
 a0 = 400.
 PN = aN**(1.5)
@@ -21,4 +29,4 @@ Pratio0 = x0**(-1.5)
 times = np.linspace(0,Tfin / PN ,Npts)
 init_pts =  get_init_points(x0,sigma_w0,Ntraj)
 histories = get_histories(init_pts,times,cmap.full_map)
-np.savez_compressed('./sim',times = PN * times, histories = histories)
+np.savez_compressed('./simresults/sim{:03d}'.format(I),times = PN * times, histories = histories)
---
+++ code/testme.py
@@ -0,0 +1,3 @@
+from multiprocessing import cpu_count
+Ncpu = cpu_count()
+print(f"running with {Ncpu} CPUs")
