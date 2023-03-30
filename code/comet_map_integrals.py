b24a4f501a026324ae0c1ff50a32f1ffbea7806b
Sam Hadden <shadden1107@gmail.com>
2023-03-30 16:46:44 -0400

    changed integral test

--- code/comet_map_integrals.py
+++ code/comet_map_integrals.py
@@ -3,8 +3,8 @@ import numpy as np
 from scipy.integrate import quad
 
 if __name__=="__main__":
-    k=3
-    q = 1.2
+    k=2
+    q = 1.5
     beta = 1/q
     ck1 = comet_map_ck(k,q)
     print("celmech: {}".format(ck1))
