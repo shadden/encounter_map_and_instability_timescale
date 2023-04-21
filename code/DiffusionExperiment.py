4483d837eacd6aa93f87a29cd327baf7aa80e44b
Sam Hadden <shadden1107@gmail.com>
2023-04-21 14:08:58 -0400

    new experiment

--- code/DiffusionExperiment.py
+++ code/DiffusionExperiment.py
@@ -17,16 +17,21 @@ else:
 
 muN = 5.15e-5
 aN = 30.
-q = 35.
-a0 = 400.
+q = 40.
+cmap = CometMap(muN,40,q/aN,kmax = 40)
+# Find "strong overlap" regime
+eps_crit = cmap.get_eps_crit(kmax = 2)
+a0 = aN * (eps_crit / 3 / cmap.m)**(2/5)
+
+
 PN = aN**(1.5)
 sigma_w0 = 0.1
 
-cmap = CometMap(muN,40,q/aN)
+
 x0  = aN/a0
 Pratio0 = x0**(-1.5)
 
 times = np.linspace(0,Tfin / PN ,Npts)
 init_pts =  get_init_points(x0,sigma_w0,Ntraj)
 histories = get_histories(init_pts,times,cmap.full_map)
-np.savez_compressed('./simresults/sim{:03d}'.format(I),times = PN * times, histories = histories)
+np.savez_compressed('./simresults/sim_q40_{:03d}'.format(I),times = PN * times, histories = histories)
