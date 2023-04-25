501b54216c9419fb82200302cf2a0f2ec3029b19
Sam Hadden <shadden1107@gmail.com>
2023-04-25 10:00:12 -0400

    code to determine last KAM curve numerically

---
+++ code/FindLastKAM.py
@@ -0,0 +1,34 @@
+from celmech.maps import CometMap
+import numpy as np
+
+muN = 5.15e-5
+aN = 30.
+PN = aN**(1.5)
+
+
+def KAM_exists(cmap,Nensemble,Niter):
+    ensemble = np.array([(0,0.5 + dw) for dw in np.random.normal(0,1e-5,Nensemble)])
+    for i in xrange(Niter):
+        for j in xrange(Nensemble):
+            ensemble[j] = cmap(ensemble[j])
+            if np.abs(ensemble[j,1]) > 1:
+                return False
+    else:
+        return True
+
+def experiment(q,Nensemble = 10,Ttraj = 5e9,return_cmap = False):
+    cmap = CometMap(muN,10,q/aN,kmax = 16)
+    epscrit = cmap.get_eps_crit(kmax = 2)
+    Ncrit = int(np.floor((epscrit/cmap.m/3)**(3/5)))
+    cmap.N = Ncrit
+    Ntraj = int( Ttraj / PN / cmap.N)
+    while not KAM_exists(cmap,Nensemble,Ntraj):
+        cmap.N -= 1
+        Ntraj = int( Ttraj / PN / cmap.N)
+    if return_cmap:
+        return q,cmap.N,cmap
+    
+    return q,cmap.N
+
+
+
