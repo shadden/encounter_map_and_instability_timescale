e0a58dc63493a105a93cef267fb1d3238eb33fcf
Sam Hadden <shadden1107@gmail.com>
2023-06-20 20:53:03 -0400

    bug fixes

--- code/EscapeTimesLocal.py
+++ code/EscapeTimesLocal.py
@@ -2,7 +2,7 @@ import numpy as np
 from celmech.maps import CometMap
 import sys
 
-savedir = "./"
+savedir = "/fs/lustre/cita/hadden/03_comet_diffusion/escape_times/"
 
 def get_local_escape_times(sigma_w0,Ntraj,itermax,comet_map, w_esc = 1):
     thetas = np.zeros(Ntraj)
@@ -13,30 +13,31 @@ def get_local_escape_times(sigma_w0,Ntraj,itermax,comet_map, w_esc = 1):
     i = 0
     while np.any(bound) and i<itermax:
         iterations[bound] = i
-        thetas[bound],ws[bound] = comet_map(np.array((thetas[bound],ws[bound]))) 
+        thetas[bound],ws[bound] = comet_map(np.array((thetas[bound],ws[bound])))
         bound = np.logical_and(np.abs(ws)<w_esc,bound)
         i+=1
     return iterations, ws, thetas, bound
 
-if __name__="__main__":
-    qKAM1,NKAM1=np.transpose(np.load("../data/last_kam_curves2.npy"))
-    qKAM2,NKAM2=np.transpose(np.load("../data/last_kam_curves.npy"))
+if __name__=="__main__":
+    datadir="/fs/lustre/cita/hadden/03_comet_diffusion/data/"
+    qKAM1,NKAM1=np.transpose(np.load(datadir+"last_kam_curves2.npy"))
+    qKAM2,NKAM2=np.transpose(np.load(datadir+"last_kam_curves.npy"))
+
+    qI = int(sys.argv[1])
+    dN = int(sys.argv[2])
+
     qs = np.concatenate((qKAM1,qKAM2))
-
-    I = int(sys.argv[1])
-    qI = int(sys.argv[2])
-    dN = int(sys.argv[3])
-
     Ncrits = np.concatenate((NKAM1,NKAM2))
     q = qs[qI]
-    N0 = Ncrits[qI] 
-
-    cmap = CometMap(5.15e-5,N0 + dN,q/35,max_kmax = 64)
-
-    iterations, ws, thetas, bound = get_local_escape_times(1e-3,4096,1_000_000,cmap)
+    N0 = Ncrits[qI]
+    print("N = {}".format(N0+dN))
+    cmap = CometMap(5.15e-5,int(N0 + dN),q/30,max_kmax = 64)
+    Ntraj = 2**15
+    Nmax = 300_000
+    iterations, ws, thetas, bound = get_local_escape_times(1e-3,Ntraj,Nmax,cmap)
     np.savez_compressed(
-        '{}/local_escape_times_q{:.2f}_dN{:03d}_{:03d}'.format(savedir,q,I),
-        iterations = iterations, 
+        '{}/local_escape_times_q{:.2f}_N{:03d}'.format(savedir,q,cmap.N),
+        iterations = iterations,
         w_values = ws,
         theta_values = thetas,
         bound = bound
