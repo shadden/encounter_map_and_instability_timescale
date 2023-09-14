08390b95a5442683f1caa4635c3fa604d29eb2af
Sam Hadden <shadden1107@gmail.com>
2023-09-14 10:25:31 -0400

    vectorize experiment

--- code/FindLastKAM.py
+++ code/FindLastKAM.py
@@ -5,18 +5,17 @@ muN = 5.15e-5
 aN = 30.
 PN = aN**(1.5)
 
-
 def KAM_exists(cmap,Nensemble,Niter):
-    ensemble = np.array([(0,0.5 + dw) for dw in np.random.normal(0,1e-5,Nensemble)])
+    ensemble = np.transpose([(0,0.5 + dw) for dw in np.random.normal(0,1e-5,Nensemble)])
     for i in xrange(Niter):
-        for j in xrange(Nensemble):
-            ensemble[j] = cmap(ensemble[j])
-            if np.abs(ensemble[j,1]) > 1:
-                return False
+        ensemble = cmap(ensemble)
+        w = ensemble[1]
+        if not np.alltrue(np.logical_and(w<1,0<w)):
+            return False
     else:
         return True
 
-def experiment(q,Nensemble = 10,Ttraj = 5e7,return_cmap = False):
+def experiment(q,Nensemble = 10,Ttraj = 5e9,return_cmap = False):
     cmap = CometMap(muN,10,q/aN,max_kmax = 64,rtol = 0.07)
     epscrit = cmap.get_eps_crit(kmax = 2)
     Ncrit = int(np.floor((epscrit/cmap.m/3)**(3/5)))
